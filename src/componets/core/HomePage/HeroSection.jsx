0ccd62eab8e38a22d991f6b3665691d88f8d93b3
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-09-24 15:19:51 +0530

    Slider Done

--- src/componets/core/HomePage/HeroSection.jsx
+++ src/componets/core/HomePage/HeroSection.jsx
@@ -1,49 +1,126 @@
-import React, { useState, useEffect } from 'react'
-const images = [
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg',
-  'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg',
-  // Add more image URLs here
-];
-export default function HeroSection() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+import React, { useState } from 'react';
 
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+const HeroSection = () => {
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  const handlePrevSlide = () => {
+    setActiveSlide((prevSlide) => (prevSlide === 0 ? 2 : prevSlide - 1));
   };
 
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  const handleNextSlide = () => {
+    setActiveSlide((prevSlide) => (prevSlide === 2 ? 0 : prevSlide + 1));
   };
-
-  // Automatically advance slide every 5 seconds
-  useEffect(() => {
-    const interval = setInterval(nextSlide, 5000);
-    return () => clearInterval(interval); // Cleanup on unmount
-  }, []);
 
   return (
-    <div >
-      <div className="relative mb-8 ">
-        <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="w-4xl align-center mx-auto" />
+    <div
+      id="carouselExampleCaptions"
+      className="relative"
+      data-te-carousel-init
+      data-te-ride="carousel"
+    >
+      {/* Carousel indicators */}
+      <div
+        className="absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
+        data-te-carousel-indicators
+      >
+        {Array.from({ length: 3 }).map((_, index) => (
+          <button
+            key={index}
+            type="button"
+            data-te-target="#carouselExampleCaptions"
+            data-te-slide-to={index}
+            data-te-carousel-active={activeSlide === index}
+            className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+            aria-current={activeSlide === index}
+            aria-label={`Slide ${index + 1}`}
+          ></button>
+        ))}
+      </div>
 
+      {/* Carousel items */}
+      <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+        {Array.from({ length: 3 }).map((_, index) => (
+          <div
+            key={index}
+            className={`relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none ${
+              activeSlide === index ? 'block' : 'hidden'
+            }`}
+            data-te-carousel-active={activeSlide === index}
+            data-te-carousel-item
+            style={{ backfaceVisibility: 'hidden' }}
+          >
+            <img
+              src={`https://tecdn.b-cdn.net/img/Photos/Slides/img%20(${index + 15}).jpg`}
+              className="block w-full"
+              alt={`Slide ${index + 1}`}
+            />
+            <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white">
+              <h5 className="text-xl">Slide  label</h5>
+              <p>Some representative placeholder content for Slide .</p>
+            </div>
+          </div>
+        ))}
+      </div>
 
-        <button onClick={prevSlide} type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-          <span className="inline-flex items-center justify-center w-14 h-14 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg className="w-8 h-8 text-blue-800 dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
-            </svg>
-            <span className="sr-only">Previous</span>
-          </span>
-        </button>
-        <button onClick={prevSlide} type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-          <span className="inline-flex items-center justify-center w-14 h-14 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg className="w-8 h-8 text-blue-800 dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-            </svg>
-            <span className="sr-only">Next</span>
-          </span>
-        </button>
-      </div></div>
-  )
-}
+      {/* Carousel controls - prev item */}
+      <button
+        className="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+        type="button"
+        data-te-target="#carouselExampleCaptions"
+        data-te-slide="prev"
+        onClick={handlePrevSlide}
+      >
+        <span className="inline-block h-8 w-8">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+          </svg>
+        </span>
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Previous
+        </span>
+      </button>
+
+      {/* Carousel controls - next item */}
+      <button
+        className="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+        type="button"
+        data-te-target="#carouselExampleCaptions"
+        data-te-slide="next"
+        onClick={handleNextSlide}
+      >
+        <span className="inline-block h-8 w-8">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </span>
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Next
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default HeroSection;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import HeroSection from '../componets/core/HomePage/HeroSection'
 
 export default function HomePage() {
   return (
     <div>
-    <h1 className="text-3xl font-bold underline text-center">   
-    HomePage
-    </h1>     
+    
+    <HeroSection/>
     </div>
   )
 }
